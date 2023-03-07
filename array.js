bcb8dd98e23474e0aa7ee34b324748b4d6097a54
Alrico Rizki <alricosaiyan123@gmail.com>
2023-03-07 20:33:13 +0700

    TASK ARRAY

---
+++ array.js
@@ -0,0 +1,17 @@
+//unshift buat masukin data ke depan
+
+
+let wadah1 = ["gula","air","garam","beras","merica","masako"]
+console.log(wadah1[1])
+console.log(wadah1[3])
+console.log(wadah1[5])
+
+let wadah2 = ["buku tulis","koran","hvs","folio","A3","majalah"]
+console.log(wadah2[1])
+console.log(wadah2[3])
+console.log(wadah2[5])
+
+let wadah3 = ["barcelon","r madrid","a madrid","r sociedad","r betis","rayo"]
+console.log(wadah3[1])
+console.log(wadah3[3])
+console.log(wadah3[5])

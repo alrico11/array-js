e902f7be3822b718a64b064bad050286c91f8e45
Alrico Rizki <alricosaiyan123@gmail.com>
2023-03-07 20:37:01 +0700

    TASK ARRAY

--- array.js
+++ array.js
@@ -1,17 +1,20 @@
 //unshift buat masukin data ke depan
 //array.length-1 buat ambil nilai terakhir
 
-let wadah1 = ["gula","air","garam","beras","merica","masako"]
+let wadah1 = ["gula","air","garam","beras","merica"]
 console.log(wadah1[1])
 console.log(wadah1[3])
-console.log(wadah1[5])
+wadah1.push("micin")
+console.log(wadah1[5],"\n")
 
-let wadah2 = ["buku tulis","koran","hvs","folio","A3","majalah"]
+let wadah2 = ["buku tulis","koran","hvs","folio","A3"]
 console.log(wadah2[1])
 console.log(wadah2[3])
-console.log(wadah2[5])
+wadah2.push("merica")
+console.log(wadah2[5],"\n")
 
-let wadah3 = ["barcelona","r madrid","a madrid","r sociedad","r betis","rayo"]
+let wadah3 = ["barcelona","r madrid","a madrid","r sociedad","r betis"]
 console.log(wadah3[1])
 console.log(wadah3[3])
-console.log(wadah3[5])
+wadah3.push("rayo")
+console.log(wadah3[5],"\n")
